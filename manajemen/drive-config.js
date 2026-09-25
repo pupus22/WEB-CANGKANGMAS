7ecb86c4a271b121db3131068f585a86f15f5fe7
pupus22 <pupusdwisuryoanggono2@gmail.com>
2026-09-25 11:54:14 +0700

    Update drive-config.js

--- manajemen/drive-config.js
+++ manajemen/drive-config.js
@@ -1,3 +1,4 @@
-// Setel Google OAuth Client ID dari Google Cloud Console. BUKAN client secret.
-// Jalur resmi dan instruksi lengkap: GOOGLE-DRIVE-SETUP.md di root paket.
-export const DRIVE_CLIENT_ID = '';
+// Google OAuth Client ID untuk Google Drive Cangkang Mas.
+// Jangan memasukkan Client Secret ke file ini.
+
+export const DRIVE_CLIENT_ID = "470615296679-2qfmivclaauf1a65g7v02sua3oab348j.apps.googleusercontent.com";
